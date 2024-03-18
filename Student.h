24fb54439c01b9af422f014b4f29f0e25db41483
amyscr <143654611+amyscr@users.noreply.github.com>
2024-03-17 22:20:29 -0400

    Create Student.h

---
+++ Student.h
@@ -0,0 +1,25 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+using namespace std;
+#include "Person.h"
+
+class Student : public Person
+{
+    public:
+        Student();
+        Student(string first, string last, int day, int month, int year);
+
+        string getMajor()const;
+        double getGPA()const;
+        void setMajor(string decision);
+        void setGPA(double update);
+
+    protected:
+        string major;
+        double GPA;
+
+    //private:
+};
+
+#endif // STUDENT_H
