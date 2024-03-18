94e68097813d01fd954c1df158fc64a45ec1092a
amyscr <143654611+amyscr@users.noreply.github.com>
2024-03-17 23:57:10 -0400

    Update main.cpp + demonstrate code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include "Person.h"
+#include "Employee.h"
+#include "Student.h"
+#include "Professor.h"
+
 
 using namespace std;
 
@@ -7,6 +11,15 @@ int main()
 {
     Person p1("Tony", "Stark", 1, 21, 1950);
     cout << p1.getName() << " born on: " << p1.getBirthdate() << endl;
+
+    Student s1("Billie", "Armstrong", 2, 17, 1972, "Music", 3.9);
+    cout << s1.getName() << " born on: " << s1.getBirthdate() << endl;
+
+    Employee e1("Jenny", "Jenny", 8, 6, 1975, 309);
+    cout << e1.getName() << " born on: " << e1.getBirthdate() << endl;
+
+    Professor t1("Gerard", "Way", 4, 9, 1977, 20, "Art");
+    cout << t1.getName() << " born on: " << t1.getBirthdate() << endl;
 
     return 0;
 }
