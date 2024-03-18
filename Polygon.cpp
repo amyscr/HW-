c05bc4dc7e88f83189c75cd742fd09b064965f7c
amyscr <143654611+amyscr@users.noreply.github.com>
2024-03-18 16:05:34 -0400

    Update Polygon.cpp

--- Polygon.cpp
+++ Polygon.cpp
@@ -1,1 +1,22 @@
+#ifndef POLYGON_H
+#define POLYGON_H
+
+class Polygon
+{
+    public:
+        Polygon();
+        Polygon(int newWidth, int newHeight);
+        void setWidth(int newWidth);
+        void setHeight(int newHeight);
+
+        virtual int getArea(int newWidth, int newHeight) const= 0;
+        virtual void draw() = 0;
+
+    protected:
+        int width = 0;
+        int height = 0;
+
+};
+
+#endif // POLYGON_H
 
