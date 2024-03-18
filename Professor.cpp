4a2d59ce6f54a9456524d882f46c151d87b8e945
amyscr <143654611+amyscr@users.noreply.github.com>
2024-03-17 23:39:40 -0400

    Update Professor.cpp + fixed

--- Professor.cpp
+++ Professor.cpp
@@ -1,9 +1,7 @@
 #include "Professor.h"
 
-Professor::Professor(): Employee(), profession("")
-{}
-Professor::Professor(string first, string last, int day, int month, int year, string subject): Person(first, last, day, month, year), profession(subject)
-{}
+Professor::Professor(): Employee(), profession(""){}
+Professor::Professor(string first, string last, int day, int month, int year, double wage, string subject): Employee(first, last,day,month,year,wage), profession(subject){}
 
 string Professor::getProfession() const{return profession;}
 void Professor::setProfession(string assignment){profession = assignment;}
