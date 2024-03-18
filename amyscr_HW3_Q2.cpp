0108844ca3346e8a71e0667ae03f4b81cc7013fa
amyscr <143654611+amyscr@users.noreply.github.com>
2024-03-18 16:05:01 -0400

    Update amyscr_HW3_Q2.cpp

--- amyscr_HW3_Q2.cpp
+++ amyscr_HW3_Q2.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
+#include "Polygon.h"
+#include "Triangle.h"
+#include "Rectangle.h"
 
 using namespace std;
 
 int main()
 {
     cout << "Hello world!" << endl;
+    Triangle t1(7,9);
+    Rectangle r1(4,6);
+    t1.draw();
+
+    r1.draw();
     return 0;
 }
-
